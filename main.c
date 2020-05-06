ca22a42a0f61a2fe8fc6afff403cacb3d6a8aa0b
Natalia <cocanmatveeva@gmail.com>
2020-05-06 21:42:24 +0300

    Отображение меню

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+int displayMenu() {
+	printf("Выберите действие:\n");
+	
+	const char *menu[] = {"One", "Two", "Three"};
+	size_t menuItemsCount = sizeof(menu)/sizeof(menu[0]);
+	for (int i = 0; i < menuItemsCount; i++) {
+		printf("%d. %s\n", i + 1, menu[i]);
+	}
+	printf("\nВаш выбор: ");
+	
+	int userChoise;
+	scanf("%d", &userChoise);
+	return userChoise;
+}
+
 int main(int argc, const char * argv[]) {
-	printf("Text");
+	displayMenu();
 	return 0;
 }
